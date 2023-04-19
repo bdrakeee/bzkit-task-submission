ffa95a0a6b081999f625711337981769f19dbc4b
Drake Buctot <d.buctot@gmail.com>
2023-04-20 00:28:57 +0800

    updated the search_users function implmenting the linear search algorithm

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,23 @@ def search_users(args):
     """
 
     # Implement search here!
+    user_index = set()
+    result = []
+    for i in range(len(USERS)):
+        if "id" in args:
+            if args["id"] == USERS[i]["id"]:
+                user_index.add(i)
+        if "name" in args:
+            if args["name"].lower() in USERS[i]["name"].lower():
+                user_index.add(i)
+        if "age" in args:
+            if USERS[i]["age"] in [int(args["age"]), int(args["age"])+1, int(args["age"])-1]:
+                user_index.add(i)
+        if "occupation" in args:
+            if args["occupation"].lower() in USERS[i]["occupation"].lower():
+                user_index.add(i)
+        
+        result = [USERS[i] for i in user_index]
 
-    return USERS
+
+    return result
